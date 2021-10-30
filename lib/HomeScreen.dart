9c6ec8545623ca720577bef8458574cfd85a4bbd
shehab <salshazlly3030@gmail.com>
2021-10-30 10:22:25 +0200

    add splash

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -5,11 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-
-        Scaffold(),
-      ],
-    );
+    return Container();
   }
 }
